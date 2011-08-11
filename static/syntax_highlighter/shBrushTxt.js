13a697710c2eb8b85b01153ae2d13333db487396
Michael Granados <michaelgranados@gmail.com>
2011-08-11 12:35:44 -0300

    Arquivos estaticos

---
+++ static/syntax_highlighter/shBrushTxt.js
@@ -0,0 +1,17 @@
+/*
+ * JsMin
+ * Javascript Compressor
+ * http://www.crockford.com/
+ * http://www.smallsharptools.com/
+*/
+
+dp.sh.Brushes.TXT=function()
+{
+	var keywords='';
+    var values='';
+    var fonts='';
+    this.regexList=[];
+    this.Style='';
+}
+dp.sh.Brushes.TXT.prototype=new dp.sh.Highlighter();
+dp.sh.Brushes.TXT.Aliases=['txt'];
